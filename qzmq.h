9ad02477964ae0a7cc705ff0d5d159627ece3bd2
Whitney.Jackson <whjackson@gmail.com>
2012-09-29 14:50:12 -0500

    include and organize all zmq funcitons in zmq.h

--- qzmq.h
+++ qzmq.h
@@ -1,7 +1,13 @@
-K q_init (K);
-K q_socket (K, K);
-K q_setsockopt (K, K, K);
 K q_bind (K, K);
+K q_close (K);
 K q_connect (K, K);
+K q_device (K); // XXX
+K q_getsockopt (K); // XXX
+K q_init (K);
 K q_send (K, K);
+K q_setsockopt (K, K, K);
+K q_socket (K, K);
+K q_term (K); // XXX
+K q_version ();
+
 K on_msg_cb (int);
